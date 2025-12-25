1c3994f8770950069329f583bdfc04bf30fe61f5
iTzNikk <nicolo.buganza@tiscali.it>
2025-12-25 22:30:28 +0100

    Update mainTemperature.adb
    Just some visuals

--- TemperatureProject/mainTemperature.adb
+++ TemperatureProject/mainTemperature.adb
@@ -1,4 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 with Temperature; use Temperature;
 
 procedure mainTemperature is
@@ -6,4 +7,10 @@ procedure mainTemperature is
    Room_Celsius : Celsius;
 begin 
    Room_Celsius := To_Celsius(Room_Temp);
+
+   Put("Temperature: ");
+   Put(Float(Room_Temp), Fore=>1, Aft=>2, Exp=>0);
+   Put(" F = ");
+   Put(Float(Room_Celsius), Fore=>1, Aft=>2, Exp=>0);
+   Put_Line(" C");
 end mainTemperature;
